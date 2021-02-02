265e799440148c041734013e51ff22823534bad7
Esma <esmakzlkaya@gmail.com>
2021-02-02 19:18:31 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,48 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Abstract;
+using Business.Abstract;
+using Entities.Concrete;
+using Entities.Abstract;
+using System;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Add(new Car { Id=4,BrandId=2,ColorId=2,ModelYear=2018,DailyPrice=300,Description="AUDI A2 DİZEL"});
+
+            Console.WriteLine("ADD -------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Update(new Car { Id = 1, DailyPrice = 500, Description = "ZAMLANDI" });
+
+            Console.WriteLine("UPDATE -------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Delete(new Car { Id=4});
+
+            Console.WriteLine("DELETE -------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=2,ModelYear=2015,DailyPrice=170,Description="RENAULT FLUENCE 2015 Dizel" },
+                new Car{Id=2,BrandId=2,ColorId=2,ModelYear=2015,DailyPrice=250,Description="AUDI A3 2013 Dizel Otomatik"},
+                new Car{Id=3,BrandId=3,ColorId=1,ModelYear=2013,DailyPrice=200,Description="VOLKSWAGEN JETTA 2013 Dizel Otomatik"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c=>c.Id==id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c=>c.Id==car.Id);
+
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
